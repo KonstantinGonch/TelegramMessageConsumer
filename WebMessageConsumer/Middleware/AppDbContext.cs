7bb36d86bd3d951239fa0d59198367aaf9d9c7ff
Goncharov Konstantin <goncharovkostya.1997@gmail.com>
2022-07-01 21:50:37 +0300

    Добавление веб-проекта

---
+++ WebMessageConsumer/Middleware/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebMessageConsumer.Models;
+
+namespace WebMessageConsumer.Middleware
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Message> Messages { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("server=82.146.56.114;user=external;password=superFinashka;database=TlgConsumer;", new MySqlServerVersion(new Version(8, 0, 11)));
+        }
+    }
+}
---
+++ WebMessageConsumer/Models/Message.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMessageConsumer.Models
+{
+    public class Message
+    {
+        public long Id { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+        public string UserName { get; set; }
+        public bool IsResponse { get; set; }
+    }
+}
